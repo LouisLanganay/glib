dbc1b8c1bf45a988c348bfe63a71d764551037c0
LouisLanganay <louis.langanay@epitech.eu>
2023-01-17 17:12:37 +0100

    feat: gl_delete_scene.c, gl_delete_scenes.c fix: gl_delete_event.c, gl_delete_events.c

--- lib/gl/scenes/gl_delete_scene.c
+++ lib/gl/scenes/gl_delete_scene.c
@@ -2,19 +2,19 @@
 ** EPITECH PROJECT, 2023
 ** glib
 ** File description:
-** gl_delete_event
+** gl_delete_scene
 */
 
 #include "../gl.h"
 
-void gl_delete_event(GLib_t *glib, int id)
+void gl_delete_scene(GLib_t *glib, int id)
 {
-    events_t *tmp = glib->events;
-    events_t *prev = NULL;
+    scenes_t *tmp = glib->scenes;
+    scenes_t *prev = NULL;
     while (tmp) {
         if (tmp->id == id) {
             if (prev == NULL)
-                glib->events = tmp->next;
+                glib->scenes = tmp->next;
             else
                 prev->next = tmp->next;
             free(tmp);
--- lib/gl/scenes/gl_delete_scenes.c
+++ lib/gl/scenes/gl_delete_scenes.c
@@ -2,19 +2,19 @@
 ** EPITECH PROJECT, 2023
 ** glib
 ** File description:
-** gl_delete_events
+** gl_delete_scenes
 */
 
 #include "../gl.h"
 
-void gl_delete_events(GLib_t *glib)
+void gl_delete_scenes(GLib_t *glib)
 {
-    events_t *tmp = glib->events;
-    events_t *next = NULL;
+    scenes_t *tmp = glib->scenes;
+    scenes_t *next = NULL;
     while (tmp != NULL) {
         next = tmp->next;
         free(tmp);
         tmp = next;
     }
-    glib->events = NULL;
+    glib->scenes = NULL;
 }
